0e951d36ef27d54794513af40049eb251c4a7572
NguyenDung2491996 <nguyenvandung24996@gmail.com>
2021-12-09 14:40:46 +0700

    Setup axios

---
+++ js/api/axiosClient.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+const axiosClient = axios.create({
+    baseURL: 'https://js-post-api.herokuapp.com/api/',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+export default axiosClient;
--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+import axiosClient from './api/axiosClient';
+import postApi from './api/postApi';
+
 console.log('Hello world');
+
+async function main() {
+    const queryParams = {
+        _page: 1,
+        _limit: 5,
+    };
+    const response = await postApi.getAll(queryParams);
+    console.log(response);
+}
+main();
